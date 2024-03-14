e976cf69c86556b3926644a8d445766b4ecbf730
Sebastian Camargo <juansecapri@hotmail.com>
2024-03-14 15:23:42 -0500

    Cambios en codigos

--- resources/views/livewire/dashboard/registro-codigos.blade.php
+++ resources/views/livewire/dashboard/registro-codigos.blade.php
@@ -3,25 +3,6 @@
         <div class="codigo-text">
             <label for="inputCodigos">Codigos: </label>
             <input type="text" wire:model="codigo" class="mb-3" id="inputCodigos">
-            
-            <label for="punto_entrega">¿D&oacute;nde obtuviste tu c&oacute;digo?</label>
-            <select id="punto_entrega" wire:model="punto_entrega" class="mt-0"> 
-                <option value="">Selecciona una opción</option>
-                <option value="Tienda de Barrio">Tienda de Barrio</option>
-                <option value="LicoExpress">LicoExpress</option>
-                <option value="Licorera">Licorera</option>
-                <option value="Restaurante">Restaurante</option>
-                <option value="TADA">TADA</option>
-                <option value="RAPPI">RAPPI</option>
-                <option value="Influencer">Influencer</option> 
-                <option value="Otro">Otro</option>
-            </select>
-
-            @error('punto_entrega')
-                <div class="error-punto-entrega">
-                    {{ $message }}
-                </div>
-            @enderror
             @error('codigo')
                 <div class="text-invalid-codigo">
                     {{ $message }}
@@ -37,11 +18,6 @@
                     {{ $message }}
                 </div>
             @enderror
-            <div class="texto-consumo-cont-codigos">
-                <p>*Te invitamos a registrar tus puntos de manera responsable. <span>Límite de puntos
-                        diario: 450 puntos.</span></p>
-
-            </div>
         </div>
  
     </div>
